ce927509c0b408c84f9e0e58ac1ae0e1c6210ac2
Carsten Winsnes <carstenw@arsnebula.com>
2015-01-06 20:34:12 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic mTurk API for Meteor
+
+The is a Meteor package for mTurk based on the node package found here:
+
+https://github.com/jefftimesten/mturk
+
+# Attribution and Acknowledgements
+
+https://github.com/jefftimesten/mturk
+https://github.com/HarvardEconCS/turkserver-meteor
+
+# License
+
+MIT
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'csats:mturk',
+  summary: 'Basic mTurk API for Meteor',
+  version: '0.1.0',
+  git: 'https://gitlab.com/csats/apps.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.0");
+  api.use("meteor-platform");
+
+  api.addFiles([
+    "lib/js/mturk.js"
+  ], ["server"])
+
+  api.export("MTurk");
+});
+
+Npm.depends({
+  "request": "2.51.0",
+  "libxmljs": "0.12.0",
+  "validator": "2.0.0",
+  "async": "0.9.0"
+})
