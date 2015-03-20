280433d3f211304263ae2651099143d982370b64
Adam Monsen <haircut@gmail.com>
2015-03-19 19:29:59 -0700

    move tests file
    This should (eventually) test more than just the constructor.
    
    FOO-tests.js is the default name for tests for a new Meteor package, so
    this seemed like a slightly better filename. I didn't see any advantage
    to using a tests/ subdir at this time.

--- package.js
+++ package.js
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
 Package.onTest(function(api){
   api.use("csats:mturk");
   api.use("tinytest");
-  api.add_files("test/constructor.js");
+  api.add_files("mturk-tests.js");
 })
 
 Npm.depends({
